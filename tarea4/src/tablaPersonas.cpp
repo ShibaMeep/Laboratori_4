6758f95384b4bb40878b7ebc21d44ec7deb27388
Shiba_Code <fantasmartu@gmail.com>
2026-06-19 15:23:30 -0300

    5: Se terminan de implementar las funciones de tablaPersonas, quedan funcionando todos los casos de prueba relacionados.

--- tarea4/src/tablaPersonas.cpp
+++ tarea4/src/tablaPersonas.cpp
@@ -4,43 +4,63 @@ struct rep_tablaPersonas {
     int n; // Tamaño de la tabla de dispersión
 };
 
-TTablaPersonas crearTTablaPersonas(int max){
-    /*TTablaPersonas h = new TTablaPersonas;
-    h->n = max;
-    h->tabla = new Lista[max];
-    for (int i = 0; i < max; i++){
-        h->tabla[i]=NULL;
+nat funcionDeDispersion(const char nombre[100]){
+    nat res = 0;
+    int i = 0;
+    while(i< 100 && nombre[i] != '\0'){
+        res += int(nombre[i]);
+        i++;
     }
-    return h;*/
-    return NULL;
+    return res;
+}
+
+TTablaPersonas crearTTablaPersonas(int max){
+    TTablaPersonas t = new rep_tablaPersonas;
+    t->n = max;
+    t->tabla = new TPersonasLDE[max];
+    for (int i = 0; i < max; i++) {
+        t->tabla[i] = crearTPersonasLDE(); // Inicializamos cada posición de la tabla con una lista vacía de personas
+    }
+    return t;
 }
 
 void insertarPersonaEnTTablaPersonas(TTablaPersonas &tabla, TPersona persona){
-    /*int pos = funcionDeDispersion(persona->nombre);
-    nodo*nuevo = new nodo;
-    nuevo->persona = persona;
-    nuevo->sig = t->tabla[pos];
-    t->tabla[pos] = nuevo; // Insertamos a una persona
-*/
+    int pos = funcionDeDispersion(nombreTPersona(persona)) % tabla->n; // Calculamos la posición en la tabla de dispersión
+    insertarTPersonasLDE(tabla->tabla[pos], persona, 1); // Insertamos la persona al inicio de la lista en esa posición
 
 }
 
 
 void eliminarPersonaDeTTablaPersonas(TTablaPersonas &tabla, const char nombre[100]){
+    int pos = funcionDeDispersion(nombre) % tabla->n; // Calculamos la posición en la tabla de dispersión
+    eliminarPersonaConNombreTPersonasLDE(tabla->tabla[pos], nombre); // Eliminamos la persona con el nombre dado de la lista en esa posición
 }
 
 bool perteneceATTablaPersonas(TTablaPersonas tabla, const char nombre[100]){
-    return false;
+    int pos = funcionDeDispersion(nombre) % tabla->n; // Calculamos la posición en la tabla de dispersión
+    return estaPersonaConNombreEnTPersonasLDE(tabla->tabla[pos], nombre); // Verificamos si la persona con el nombre dado pertenece a la lista en esa posición
 }
 
 TPersona obtenerPersonaDeTTablaPersonas(TTablaPersonas tabla, const char nombre[100]){
-    return NULL;
+    int pos = funcionDeDispersion(nombre) % tabla->n; // Calculamos la posición en la tabla de dispersión
+    return obtenerPersonaConNombreTPersonasLDE(tabla->tabla[pos], nombre); // Devolvemos la persona con el nombre dado de la lista en esa posición
 }
 
 
 void liberarTTablaPersonas(TTablaPersonas &tabla){
+    if(tabla != NULL){
+        for(int i = 0; i < tabla->n; i++){
+            liberarTPersonasLDE(tabla->tabla[i]); // Liberamos cada lista de personas en la tabla
+        }
+        delete[] tabla->tabla; // Liberamos la memoria de la tabla de dispersión
+        delete tabla; // Liberamos la memoria de la estructura de la tabla
+        tabla = NULL; // Evitamos que el puntero quede colgando
+    }
 }
 
 
 void imprimirTTablaPersonas(TTablaPersonas tabla){
+    for(int i = 0; i < tabla->n; i++){
+        imprimirTPersonasLDE(tabla->tabla[i]); // Imprimimos cada lista de personas en la tabla
+    }
 }
