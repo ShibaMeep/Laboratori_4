267d2fb92dfa60f93a483f8759d5958ae68a3096
Shiba_Code <fantasmartu@gmail.com>
2026-06-20 19:31:30 -0300

    6: Se empieza a hacer la parte de colas

--- tarea4/src/colaDePrioridadPersona.cpp
+++ tarea4/src/colaDePrioridadPersona.cpp
@@ -3,11 +3,49 @@
 #include "../include/evento.h"
 
 struct rep_colaDePrioridadPersona {
-
+TPersona* cola;
+nat* posiciones;
+nat tope;
+nat cantidad;
 };
 
-TColaDePrioridadPersona crearCP(nat N) {
+
+void filtrado_ascendente(nat pos, TColaDePrioridadPersona &cp){
+
+}
+
+
+TFecha fecha_prioritaria(TColaDePrioridadPersona cp){
+  TFecha fecha = NULL;
+    nat j = 2;
+    nat k = 1;
+    for(nat i = 1; i <= cp->cantidad; i++){
+      if(!esVaciaAgendaLS()){
+
+      }
+    }
+
   return NULL;
+}
+
+TColaDePrioridadPersona crearCP(nat N) {
+  //reservamos memoria para la estructura 
+  TColaDePrioridadPersona cola = new rep_colaDePrioridadPersona; //Theta 1
+  cola->tope = N; //Theta 1
+  cola->cantidad = 0; //Theta 1
+
+  //reservamos memoria para la cola 
+  cola->cola = new TPersona[N +1 ]; //Theta N
+
+  //reservamos memoria para las posiciones
+  cola->posiciones = new nat[N + 1]; //Theta N
+
+
+  //inicializamos las posiciones
+  for (nat i = 1; i <= N; i++){
+    cola->posiciones[i] = 0; //Theta N
+  }
+  return cola;
 } 
 
 void invertirPrioridad(TColaDePrioridadPersona &cp) {
